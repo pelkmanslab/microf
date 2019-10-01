8f79b1516885382b7f8ca331353b284771c22461
Riccardo Murri <riccardo.murri@gmail.com>
2019-10-01 14:33:44 +0200

    Switch to a single-file Python script.
    No need to keep an entire package structure around if everything is
    self-contained and -basically- without external dependencies.

--- setup.py
+++ setup.py
@@ -3,10 +3,14 @@ from setuptools import setup
 setup(
     name='mf',
     version='1.0.2',
-    packages=['mf'],
+    py_modules=['mf'],
     entry_points={
-        'console_scripts': ['mf=mf.__main__:main'],
+        'console_scripts': ['mf=mf:main'],
     },
+    # `zip_safe` can ease deployment, but is only allowed if the package
+    # do *not* do any __file__/__path__ magic nor do they access package data
+    # files by file name (use `pkg_resources` instead).
+    zip_safe=True,
     ## metadata for PyPI uploads
     description='Simple image file utilities',
     url='https://github.com/pelkmanslab/mf.git',
@@ -19,8 +23,11 @@ setup(
         'riccardo.murri@gmail.com',
     ]),
     license='MIT',
-    # `zip_safe` can ease deployment, but is only allowed if the package
-    # do *not* do any __file__/__path__ magic nor do they access package data
-    # files by file name (use `pkg_resources` instead).
-    zip_safe=True,
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: Implementation :: CPython",
+    ],
 )
